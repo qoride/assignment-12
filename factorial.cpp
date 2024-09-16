6e68703bc775d3a3697b4d3823e72eb2952e855b
qoride <qoride@gmail.com>
2024-09-16 11:31:03 -0700

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int factorialRecursive(int n){
+    if(n==1){return 1;}
+    return n*factorialRecursive(n-1);
+}
+
+int factorialForLoop(int n){
+    int factorial = 1;
+    for(int i = n; i > 1; i--){
+        factorial *= n;
+    }
+    return factorial;
+}
+
+using namespace std;
+
+int main(){
+    int input;
+    while(input < 1){
+        cout << "Enter a positive integer: ";
+        cin >> input;
+        if(input < 1){
+            cout << "\n\n" << input << " is not a valid number.\n\n";
+        }
+    }
+
+    cout << "\n\nThe factorial of " << input << " is " << factorialRecursive(input);
+
+    return 0;
+}
