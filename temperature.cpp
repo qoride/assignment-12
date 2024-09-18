6c0a7da585e2b6646deb4f278937f9f421035aa6
qoride <qoride@gmail.com>
2024-09-18 18:04:59 +0000

    temperature commit

--- temperature.cpp
+++ temperature.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 int main(){
-    string temperatures;
-    cout << "Enter a list of temperatures seperated by spaces: ";
-    
+    int input,average = 0,i=0;
+
+    cout << "Enter a list of temperatures seperated by spaces and end it with any character: ";
+    while(cin >> input){
+        average += input;
+        i++;
+    }
+    average /= i;
+
+    cout << "\n\nThe average temperature is: " << average << " degrees.";
 
     return 0;
 }
